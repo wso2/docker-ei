e6847287fd89856ef05ecb8c2b10734a771f11c3
Chamindi Abayadeera <chamindi@wso2.com>
2018-09-24 16:34:55 +0530

    Change analytics docker and init.sh files V.6.4.0

--- dockerfiles/ubuntu/analytics/init.sh
+++ dockerfiles/ubuntu/analytics/init.sh
@@ -25,27 +25,11 @@ artifact_volume=${WORKING_DIRECTORY}/wso2-artifact-volume
 user=wso2carbon
 group=wso2
 
-# capture Docker container IP from the container's /etc/hosts file
-docker_container_ip=$(awk 'END{print $1}' /etc/hosts)
-
 # check if the WSO2 non-root user home exists
 test ! -d ${WORKING_DIRECTORY} && echo "WSO2 Docker non-root user home does not exist" && exit 1
 
 # check if the WSO2 product home exists
 test ! -d ${WSO2_SERVER_HOME} && echo "WSO2 Docker product home does not exist" && exit 1
-
-# copy the backed up artifacts from ${HOME}/wso2-tmp/analytics
-# copying the initial artifacts to ${HOME}/wso2-tmp/analytics was done in the Dockerfile
-# this is to preserve the initial artifacts in a volume mount (the mounted directory can be empty initially)
-# the artifacts will be copied to the <WSO2_SERVER_HOME>/wso2/analytics/conf/analytics location,
-# before the server is started
-if test -d ${temp_persisted_artifacts}; then
-    if [ -z "$(ls -A ${original_persisted_artifacts}/)" ]; then
-	    # if no artifacts under <WSO2_SERVER_HOME>/wso2/analytics/conf/analytics; copy them
-        echo "Copying shared server artifacts from temporary location to the original server home location..."
-        cp -R ${temp_persisted_artifacts}/* ${original_persisted_artifacts}
-    fi
-fi
 
 # copy any configuration changes mounted to config_volume
 test -d ${config_volume}/ && cp -RL ${config_volume}/* ${WSO2_SERVER_HOME}/
