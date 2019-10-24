7ebbb66752ada366e558824ad5b1c7cb4efdeaf0
Aaquiff <aaquiff@wso2.com>
2019-10-24 12:17:37 +0530

    Revert "Remove artifact mounting"
    This reverts commit ebee6a9a0bfadfeac6365d12bfa135a1ce3181df.

--- dockerfiles/centos/streaming-integrator/docker-entrypoint.sh
+++ dockerfiles/centos/streaming-integrator/docker-entrypoint.sh
@@ -19,6 +19,7 @@ set -e
 
 # volume mounts
 config_volume=${WORKING_DIRECTORY}/wso2-config-volume
+artifact_volume=${WORKING_DIRECTORY}/wso2-artifact-volume
 
 # check if the WSO2 non-root user home exists
 test ! -d ${WORKING_DIRECTORY} && echo "WSO2 Docker non-root user home does not exist" && exit 1
@@ -28,6 +29,8 @@ test ! -d ${WSO2_SERVER_HOME} && echo "WSO2 Docker product home does not exist" 
 
 # copy any configuration changes mounted to config_volume
 test -d ${config_volume} && [[ "$(ls -A ${config_volume})" ]] && cp -RL ${config_volume}/* ${WSO2_SERVER_HOME}/
+# copy any artifact changes mounted to artifact_volume
+test -d ${artifact_volume} && [[ "$(ls -A ${artifact_volume})" ]] && cp -RL ${artifact_volume}/* ${WSO2_SERVER_HOME}/
 
 # start WSO2 server
 sh ${WSO2_SERVER_HOME}/bin/server.sh "$@"
