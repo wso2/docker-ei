ac4c785908c5c6648939fff940d4fa335be05f77
maanadev <maanafunedu@gmail.com>
2020-07-30 15:45:02 +0530

    [7.1.0] Add WSO2 MI Monitoring Dashboard 1.2.0 Centos Docker resources

---
+++ dockerfiles/centos/monitoring-dashboard/docker-entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# ------------------------------------------------------------------------
+# Copyright 2020 WSO2, Inc. (http://wso2.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+# ------------------------------------------------------------------------
+
+set -e
+
+# volume mount
+config_volume=${WORKING_DIRECTORY}/wso2-config-volume
+
+# check if the WSO2 non-root user home exists
+test ! -d ${WORKING_DIRECTORY} && echo "WSO2 Docker non-root user home does not exist" && exit 1
+
+# check if the WSO2 product home exists
+test ! -d ${WSO2_SERVER_HOME} && echo "WSO2 Docker product home does not exist" && exit 1
+
+# copy any configuration changes mounted to config_volume
+test -d ${config_volume} && [[ "$(ls -A ${config_volume})" ]] && cp -RL ${config_volume}/* ${WSO2_SERVER_HOME}/
+
+# start WSO2 server
+sh ${WSO2_SERVER_HOME}/bin/dashboard.sh "$@"
